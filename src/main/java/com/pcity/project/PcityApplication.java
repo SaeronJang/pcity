f593d90efa4bb06688b379edb0493a9d23bcb075
SaeronJang <yoonseo35@gmail.com>
2024-10-11 15:28:36 +0900

    초기셋팅

---
+++ src/main/java/com/pcity/project/PcityApplication.java
@@ -0,0 +1,13 @@
+package com.pcity.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcityApplication.class, args);
+    }
+
+}
